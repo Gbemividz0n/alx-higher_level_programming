622645605141904ea6fbae294de35c5121a8bd60
Gbemividz0n <43783192+Gbemividz0n@users.noreply.github.com>
2022-06-03 06:20:22 +0100

    tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for ch in reversed(range(97, 123)):
-    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
+for a in reversed(range(ord("a"), ord("z") + 1)):
+    if a % 2 != 0:
+        a = a - 32
+    print("{:c}".format(a), end="")
