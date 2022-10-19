a9c71aec2f9e98304be83fd2c7592d667978ca24
Opeyemi James <43783192+Gbemividz0n@users.noreply.github.com>
2022-10-19 05:50:25 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request.get('https://swapi-api.hbtn.io/api/films/:id' + process.argv[2] + '/', function (err, response, body) {
-  if (err) throw err;
-  else if (response.statusCode === 200) {
-    console.log(JSON.parse(body).title);
-  }
+let url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
